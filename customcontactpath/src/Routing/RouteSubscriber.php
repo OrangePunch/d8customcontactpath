c19540987afeb23a373a4fa8846d952cd04a6d73
Tony Barker <4941711+tony-itfc@users.noreply.github.com>
2018-07-06 12:07:44 +0100

    Added the module files

---
+++ customcontactpath/src/Routing/RouteSubscriber.php
@@ -0,0 +1,25 @@
+<?php
+// Save this file as `src/Routing/RouteSubscriber.php` in your module directory.
+
+namespace Drupal\customcontactpath\Routing;
+
+use Drupal\Core\Routing\RouteSubscriberBase;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Listens to the dynamic route events.
+ */
+class RouteSubscriber extends RouteSubscriberBase {
+    /**
+     * {@inheritdoc}
+     */
+    public function alterRoutes(RouteCollection $collection) {
+        // Change the core's default path for `/contact` so that we can use it as an alias.
+        if ($route = $collection->get('contact.site_page')) {
+            $route->setPath('/connect');
+        }
+        if ($route = $collection->get('entity.contact_form.canonical')) {
+            $route->setPath('/connect/{contact_form}');
+        }
+    }
+}
